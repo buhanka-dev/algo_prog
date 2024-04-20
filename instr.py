cbd1be2e5315de324b5f7719cf153acb6f35ab7e
buhanka-dev <166924569+buhanka-dev@users.noreply.github.com>
2024-04-20 15:01:29 +0500

    .

--- instr.py
+++ instr.py
@@ -1,3 +1,6 @@
 description_text = 'описание'
 button_start_text = 'дальше ->'
 introduction_text = 'приветствие'
+
+result_text = 'Индекс Руфье: '
+heart_text = 'Работаспособность сердца: '
--- main.py
+++ main.py
@@ -4,25 +4,44 @@ from instr import *
 import sys
 
 
+class ScreenFinal(QWidget):
+    def __init__(self):
+        super().__init__()
+        result_label = QLabel(result_text)
+        heart_label = QLabel(heart_text)
+
+        vertical_line = QVBoxLayout()
+
+        vertical_line.addWidget(result_label, alignment=Qt.AlignCenter)
+        vertical_line.addWidget(heart_label, alignment=Qt.AlignCenter)
+
+        self.setLayout(vertical_line)
+
+
 class ScreenMain(QWidget):
     def __init__(self):
         super().__init__()
-        start_button = QPushButton(button_start_text)
-        intrduction = QLabel(introduction_text)
-        description = QLabel(description_text)
+        start_button_label = QPushButton(button_start_text)
+        intrduction_label = QLabel(introduction_text)
+        description_label = QLabel(description_text)
 
-        description.setWordWrap(True)
+        description_label.setWordWrap(True)
 
-        vertical_line1 = QVBoxLayout()
-        vertical_line1.addWidget(intrduction)
-        vertical_line1.addWidget(description)
-        vertical_line1.addWidget(start_button, alignment=Qt.AlignCenter)
+        vertical_line = QVBoxLayout()
+        vertical_line.addWidget(intrduction_label)
+        vertical_line.addWidget(description_label)
+        vertical_line.addWidget(start_button_label, alignment=Qt.AlignCenter)
 
-        self.setLayout(vertical_line1)
+        self.setLayout(vertical_line)
 
 
 app = QApplication(sys.argv)
 main_window = ScreenMain()
 main_window.setFixedSize(400, 350)
 main_window.show()
+
+final_window = ScreenFinal()
+final_window.setFixedSize(400, 350)
+final_window.show()
+
 sys.exit(app.exec_())
