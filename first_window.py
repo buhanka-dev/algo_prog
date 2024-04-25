ae91f8d2de30d6236e5aafd4f74a8a7a4227722a
buhanka-dev <166924569+buhanka-dev@users.noreply.github.com>
2024-04-25 13:21:32 +0500

    Update first_window.py

--- first_window.py
+++ first_window.py
@@ -7,7 +7,7 @@ import sys
 class FirstScreen(QWidget):
     def __init__(self, next_window):
         super().__init__()
-        self.next_window = next_window
+        self.next_w = next_window
         self.window_appearence()
         self.init_ui()
         self.connections()
@@ -15,19 +15,19 @@ class FirstScreen(QWidget):
 
 
     def window_appearence(self):
-        self.setFixedSize(500, 500)
+        self.setFixedSize(800, 500)
 
     def init_ui(self):
         self.start_button = QPushButton(button_start_text)
-        self.intrduction_label = QLabel(introduction_text)
-        self.description_label = QLabel(description_text)
+        self.intrduction_label = QLabel('<span style="font-size:15pt; font-weight: bold;">' + introduction_text + '</span>')
+        self.description_label = QLabel('<span style="font-size:15pt; ">' + description_text + '</span>')
 
         self.description_label.setWordWrap(True)
 
         self.vertical_line = QVBoxLayout()
+
         self.vertical_line.addWidget(self.intrduction_label)
         self.vertical_line.addWidget(self.description_label)
-
         self.vertical_line.addWidget(self.start_button, alignment=Qt.AlignCenter)
 
         self.setLayout(self.vertical_line)
@@ -37,10 +37,11 @@ class FirstScreen(QWidget):
 
     def next_window(self):
         self.hide()
+        self.next_w.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = FirstScreen()
+    main_window = FirstScreen('')
 
     sys.exit(app.exec_())
